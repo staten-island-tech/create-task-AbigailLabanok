9fecbb2af3ef9ae9569c39705ca3c831ec1c6768
AbigailLabanok <abigail.labanok@gmail.com>
2023-02-14 13:07:39 -0500

    asdasdasd

--- ap-csp-create-task/js/agent.js
+++ ap-csp-create-task/js/agent.js
@@ -3,11 +3,38 @@ import { DOMSelectors } from "./DOM";
 const agentapi = "https://valorant-api.com/v1/agents";
 
 async function getAgent() {
+  var things = [
+    "Brimstone",
+    "Phoenix",
+    "Sage",
+    "Sova",
+    "Viper",
+    "Cypher",
+    "Reyna",
+    "Killjoy",
+    "Breach",
+    "Omen",
+    "Jett",
+    "Raze",
+    "Skye",
+    "Yoru",
+    "Astra",
+    "Kay/o",
+    "Chamber",
+    "Neon",
+    "Fade",
+    "Harbor",
+  ];
+  var thing = things[Math.floor(Math.random() * things.length)];
+  console.log(thing);
+
   try {
-    console.log(agentapi);
-  } catch (error) {
+    const response = await fetch(agentapi);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
     console.error(err);
   }
 }
 
-getAgent();
+export { getAgent };
--- ap-csp-create-task/js/main.js
+++ ap-csp-create-task/js/main.js
@@ -1,4 +1,8 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./DOM";
+import { getAgent } from "./agent";
+
+DOMSelectors.agentBtn.addEventListener("click", getAgent);
+
 
 //https://stackoverflow.com/questions/13237421/how-to-generate-random-words-in-javascript
