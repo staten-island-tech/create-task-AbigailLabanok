89b6db296e8ab4a7d506af5fa351a5f2c847e7ee
AbigailLabanok <abigail.labanok@gmail.com>
2023-02-27 17:40:58 -0500

    ss

--- ap-csp-create-task/js/main.js
+++ ap-csp-create-task/js/main.js
@@ -1,12 +1,13 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./DOM";
 import { getAgent } from "./agent";
+import { reset } from "./reset";
 
 function getCard() {
-    getAgent();
+  getAgent();
 }
 
 DOMSelectors.agentBtn.addEventListener("click", getCard);
-
+DOMSelectors.resetBtn.addEventListener("click", reset);
 
 //https://stackoverflow.com/questions/13237421/how-to-generate-random-words-in-javascript
---
+++ ap-csp-create-task/js/reset.js
@@ -0,0 +1,7 @@
+import { DOMSelectors } from "./DOM";
+
+function reset() {
+  DOMSelectors.display.innerHTML = "";
+}
+
+export { reset };
