8c7334f7cabaae06201816da14f7c50311454f8c
AbigailLabanok <abigail.labanok@gmail.com>
2023-02-27 19:28:04 -0500

    im lost :D

--- ap-csp-create-task/js/agent.js
+++ ap-csp-create-task/js/agent.js
@@ -1,4 +1,5 @@
 import { DOMSelectors } from "./DOM";
+import { agentArr } from "./array";
 
 const agentapi = "https://valorant-api.com/v1/agents";
 
@@ -27,24 +28,34 @@ async function getAgent() {
     "Harbor",
   ];
   var agent = names[Math.floor(Math.random() * names.length)];
-  console.log(agent);
-  DOMSelectors.display.insertAdjacentHTML("beforeend", `<h2>${agent}</h2>`);
 
-  try {
-    const response = await fetch(agentapi);
-    const data = await response.json();
-    console.log(data);
-    data.data
-      .filter((element) => element.displayName.includes(agent))
-      .forEach((el) => {
-        DOMSelectors.display.insertAdjacentHTML(
-          "beforeend",
-          `<img class="agent-picture ${el.developerName}  " id="${el.displayName}" src="${el.fullPortrait}" alt="${el.displayName} card"/>`
-        );
-      });
-  } catch (err) {
-    console.error(err);
+  if (agentArr.includes(`${agent}`)) {
+    console.log("already seen it");
+  } else {
+    console.log("new");
+
+    console.log(agent);
+    DOMSelectors.display.insertAdjacentHTML("beforeend", `<h2>${agent}</h2>`);
+
+    try {
+      const response = await fetch(agentapi);
+      const data = await response.json();
+      console.log(data);
+      data.data
+        .filter((element) => element.displayName.includes(agent))
+        .forEach((el) => {
+          DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<img class="agent-picture ${el.developerName}  " id="${el.displayName}" src="${el.fullPortrait}" alt="${el.displayName} card"/>`
+          );
+        });
+    } catch (err) {
+      console.error(err);
+    }
   }
+
+  agentArr.push(`${agent}`);
+  console.log(agentArr);
 }
 
 export { getAgent };
--- ap-csp-create-task/js/reset.js
+++ ap-csp-create-task/js/reset.js
@@ -1,7 +1,10 @@
 import { DOMSelectors } from "./DOM";
+import { agentArr } from "./array";
 
 function reset() {
   DOMSelectors.display.innerHTML = "";
+  agentArr.length = 0;
+  console.log(agentArr);
 }
 
 export { reset };
